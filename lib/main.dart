847915add9c51996f604b5ed90ad06e2a03df38b
adamalcantara <alcantaradevelopment@gmail.com>
2024-08-31 10:25:16 -0400

    All sounds playing within colored blocks

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,102 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(child: TextButton(
-            onPressed: () {
-              print('Clicked');
-              final player = AudioPlayer();
-              player.play(AssetSource('note1.wav'));
-            },
-              child: Text('Click Me'),
-          ),),
+          child: Column(
+            children: [
+              Container(
+                height: 100.0,
+                width: 200.0,
+                color: Colors.red,
+                child: TextButton(
+                  onPressed: () {
+                    print('Clicked');
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note1.wav'));
+                  },
+                    child: Text(''),
+                ),
+              ),
+              Container(
+                height: 100.0,
+                width: 200.0,
+                color: Colors.orange,
+                child: TextButton(
+                  onPressed: () {
+                    print('Clicked');
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note2.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Container(
+                height: 100.0,
+                width: 200.0,
+                color: Colors.yellow,
+                child: TextButton(
+                  onPressed: () {
+                    print('Clicked');
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note3.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Container(
+                height: 100.0,
+                width: 200.0,
+                color: Colors.green,
+                child: TextButton(
+                  onPressed: () {
+                    print('Clicked');
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note4.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Container(
+                height: 100.0,
+                width: 200.0,
+                color: Colors.teal,
+                child: TextButton(
+                  onPressed: () {
+                    print('Clicked');
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note5.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Container(
+                height: 100.0,
+                width: 200.0,
+                color: Colors.blue,
+                child: TextButton(
+                  onPressed: () {
+                    print('Clicked');
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note6.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Container(
+                height: 100.0,
+                width: 200.0,
+                color: Colors.purple,
+                child: TextButton(
+                  onPressed: () {
+                    print('Clicked');
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note7.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+            ],
+          ),
+
         ),
       ),
     );
